ce085fc3ce91a2b45375233a6e419f34906c3cd0
mmiasojedow <michal@miasojedow.com>
2021-03-31 18:11:13 +0200

    basic.html and minor changes

--- moviedb_project/urls.py
+++ moviedb_project/urls.py
@@ -1,4 +1,4 @@
-"""film_project URL Configuration
+"""moviedb_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_app.views import MainView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainView.as_view(), name='main'),
 ]
