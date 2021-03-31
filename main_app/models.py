9ca8c418b9e0a63b0c5d2b99914050e73d2d5ad1
mmiasojedow <michal@miasojedow.com>
2021-03-31 22:42:59 +0200

    movie and tag models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    rating = models.IntegerField()
+    link = models.URLField(max_length=200, unique=True)
+    note = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField('Tag')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
